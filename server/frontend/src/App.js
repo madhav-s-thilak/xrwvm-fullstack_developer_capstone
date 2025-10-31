57ca2c5c388bef23180f1e63935b8af84f3b18b2
Madhav <f20221633@pilani.bits-pilani.ac.in>
2025-10-31 16:40:13 -0400

    new changes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,16 @@
-import LoginPanel from "./components/Login/Login"
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<LoginPanel />} />
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
+
 export default App;
