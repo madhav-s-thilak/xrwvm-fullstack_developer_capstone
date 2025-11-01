cb7c1ad1e4de74374a27862c9e486bd107db0a1f
Madhav <f20221633@pilani.bits-pilani.ac.in>
2025-11-01 05:16:22 -0400

    New additions

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,57 @@
-# Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
-
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    """
+    CarMake model to store information about car manufacturers
+    """
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country = models.CharField(max_length=100, default="USA")
+    founded_year = models.IntegerField(default=2000)
+    
+    class Meta:
+        verbose_name_plural = "Car Makes"
+    
+    def __str__(self):
+        return self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    """
+    CarModel model to store information about car models
+    """
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('HATCHBACK', 'Hatchback'),
+        ('TRUCK', 'Truck'),
+        ('CONVERTIBLE', 'Convertible'),
+    ]
+    
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(
+        max_length=15,
+        choices=CAR_TYPES,
+        default='SEDAN'
+    )
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MinValueValidator(2015),
+            MaxValueValidator(2023)
+        ]
+    )
+    features = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    
+    class Meta:
+        verbose_name_plural = "Car Models"
+    
+    def __str__(self):
+        return f"{self.car_make.name} - {self.name} ({self.year})"
